2663ac435cef20879e3130a4e364c1b10057a1e8
Austin Willoughby <arwigm@rit.edu>
2020-04-02 15:43:13 -0400

    testing error

--- server/test/utils/index.js
+++ server/test/utils/index.js
@@ -1,5 +1,24 @@
 // const config = require('../test_config.js');
-// const utils = require('../../src/utils');
+const utils = require('../../src/utils');
 //
 // const { limits } = config;
 //
+
+
+describe('utils', () => {
+  describe('add', () => {
+    it('should return addition of two numbers', (done) => {
+      const sum = utils.add(10, 5);
+      sum.should.equal(14);
+      done();
+    });
+  });
+
+  describe('subtract', () => {
+    it('should return subtraction of two numbers', (done) => {
+      const diff = utils.sub(10, 5);
+      diff.should.equal(5);
+      done();
+    });
+  });
+});
